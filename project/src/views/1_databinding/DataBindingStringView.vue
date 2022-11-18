ced3b8ac9887c657e52e0d5a96ffa7ccc4335e23
DESKTOP-TNO14I7\admin <jskjw157@gmail.com>
2022-11-18 17:45:32 +0900

    [ADD] 파일 추가

---
+++ project/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Hello {{ userName }}</h1>
+    <p></p>
+  </div>
+</template>
+
+<script>
+// 1. html 코드는 template tag 에 작성한다. 모든 tag에는 꼭 최상위 태그가 있어야된다.
+// 2. style tag에 scoped 속성을 정의하면 해당 css는 해당 vue파일에만 적용된다.
+export default {
+  data() {
+    return {
+      userName: 'John Doe',
+      message: 'Welcome'
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
