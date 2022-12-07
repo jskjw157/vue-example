65e91c8e8c239e6ef8b6262bd32a925082edbfa1
johny <jskjw157@gmail.com>
2022-12-07 22:29:48 +0900

    [ADD][데이터바인딩] style 바인딩 예제

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -63,6 +63,21 @@ const routes = [
     path: '/databinding/class',
     name: 'DataBindingClassView',
     component: () => import(/* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingClassView.vue')
+  },
+  {
+    path: '/databinding/style',
+    name: 'DataBindingStyleView',
+    component: () => import(/* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingStyleView.vue')
+  },
+  {
+    path: '/event/click',
+    name: 'EventClickView',
+    component: () => import(/* webpackChunkName: "event", webpackPrefetch:true */ '../views/2_event/EventClickView.vue')
+  },
+  {
+    path: '/event/change',
+    name: 'EventChangeView',
+    component: () => import(/* webpackChunkName: "event", webpackPrefetch:true */ '../views/2_event/EventChangeView.vue')
   }
 ]
 
---
+++ project/src/views/1_databinding/DataBindingStyleView.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div style="color:red; font-size:30px">스타일 바인딩</div>
+        <div :style="style1">스타일 바인딩</div>
+        <button @click="style1.color='blue'">색상바꾸기</button>
+    </div>
+</template>
+<script>
+export default {
+    components: {},
+    data() {
+        return {
+            style1: {
+                color: 'red',
+                fontSize: '30px'
+            }
+        }
+    },
+    setup() {
+    },
+    created() {
+    },
+    mounted() {
+    },
+    unmounted() {
+    },
+    methods: {
+    }
+}
+</script>
+<style scoped>
+.active {
+    background-color: greenyellow;
+    font-weight: bold;
+}
+.text-red {
+    color: red;
+}
+</style>
