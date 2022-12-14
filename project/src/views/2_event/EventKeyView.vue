fa8c70bf7b28c987f44ed5a7889e94ac4420128f
johny <jskjw157@gmail.com>
2022-12-15 00:04:42 +0900

    [ADD][이벤트] keyup event 예제

---
+++ project/src/views/2_event/EventKeyView.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <!-- <input type="search" @keyup="checkEnter($event)" v-model="searchText"> -->
+        <input type="search" @keyup.enter="doSearch" v-model="searchText">
+        <button @click="doSearch">조회</button>
+        <button type="submit" @click.prevent="doSearch">조회</button>
+    </div>
+</template>
+<script>
+// .enter
+// .tab
+// .delete
+// .esc
+// .space
+// .up
+// .down
+// .left
+// .right
+// .stop - event.stopPropagation()
+// .prevent - event.preventDefault()
+export default {
+    components: {},
+    data() {
+        return {
+            searchText: ''
+        }
+    },
+    setup() {
+    },
+    created() {
+    },
+    mounted() {
+    },
+    unmounted() {
+    },
+    methods: {
+        doSearch() {
+            console.log(this.searchText)
+        },
+        checkEnter(event) {
+            if (event.keyCode === 13) {
+                this.doSearch()
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+</style>
